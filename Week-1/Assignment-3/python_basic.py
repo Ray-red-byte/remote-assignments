c8d14dd40eff0e00b69dc50cbedf3ea43c3e48cf
Ray <ray67672@gmail.com>
2024-01-13 23:39:11 +0800

    change

--- Week-1/Assignment-3/python_basic.py
+++ Week-1/Assignment-3/python_basic.py
@@ -1,22 +1,30 @@
 def find_max(numbers):
     max_num = numbers[0]
+
     for i in numbers:
         if i > max_num:
             max_num = i
 
     return max_num
 
-def find_position(numbers, target):
+
+def find_position(numbers,target):
     position = -1
     count = 0
+    
     for i in numbers:
         if i == target:
             position = count
             break
         else:
             count += 1
+
     return position
 
 
-print(find_max([1, 2, 3, 4, 5, 8, 0, 4])) 
+print(find_max([1, 2, 3, 4, 5, 8, 0, 4]))
+print(find_max([1, 2, 3, 4, 5, 8, 0, 4]))
+print(find_max([1, 2, 3, 4, 5, 8, 0, 4]))
+print(find_position([1, 2, 3, 4, 5, 8, 0, 4], 0))
+print(find_position([1, 2, 3, 4, 5, 8, 0, 4], 0))
 print(find_position([1, 2, 3, 4, 5, 8, 0, 4], 0))
