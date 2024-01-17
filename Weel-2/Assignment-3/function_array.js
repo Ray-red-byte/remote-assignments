162335b46fc2f45595e776418f48569d621fb3b2
Ray <ray67672@gmail.com>
2024-01-17 10:59:38 +0800

    Update

---
+++ Weel-2/Assignment-3/function_array.js
@@ -0,0 +1,32 @@
+function max(numbers) {
+    if (numbers.length === 0){
+        return 'Empty List'
+    }
+
+    var max = numbers[0]
+    for (number of numbers){
+        if (number > max){
+            max = number
+        }
+    }
+    return max
+}
+
+
+function findPosition(numbers, target) {
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] === target){
+            return i
+        }
+    }
+
+    return -1
+}
+
+
+
+console.log( max([1, 2, 4, 5]) ); // should print 5
+console.log( max([5, 2, 7, 1, 6]) ); // should print 7
+console.log( findPosition([5, 2, 7, 1, 6], 5) ); // should print 0
+console.log( findPosition([5, 2, 7, 1, 6], 7) ); // should print 2
+console.log( findPosition([5, 2, 7, 7, 7, 1, 6], 7) );
