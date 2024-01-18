2f9b31ee13d454cf07ea4460cf6749cbeab0a4cd
Ray <ray67672@gmail.com>
2024-01-18 23:38:01 +0800

    Update

---
+++ Weel-2/Assignment-2/index.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btn");
+console.log(btn)
+
+
+btn.addEventListener("click", () => {
+    const lowerContent = document.querySelector(".all-containers-2");
+    lowerContent.style.display = "flex";
+    lowerContent.style.flexFlow = "wrap";
+    lowerContent.style.justifyContent = "center";
+    lowerContent.style.gap = "10px";
+    lowerContent.style.marginLeft = "auto";
+    lowerContent.style.marginRight = "auto";
+    lowerContent.style.marginTop = "15px";
+});
