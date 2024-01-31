a27c3c14f3b8ca0507184f28922c7ae6622003de
Ray <ray67672@gmail.com>
2024-01-31 16:14:15 +0800

    Create Assignment 1, 2

---
+++ Week-4/Assignment-2.js
@@ -0,0 +1,47 @@
+function ajax(src, callback){
+    // your code here
+    fetch(src)
+    .then(function(response){
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }).then(function (data) {
+        callback(data);
+    }).catch(function (error) {
+        console.error('Fetch error:', error);
+    });
+}
+
+
+function render(data){
+    // document.createElement() and appendChild() methods are preferred.
+    let products = document.createElement('div')
+    for (let item in data) {
+        console.log(data[item])
+        let each_product = document.createElement('div')
+        
+        let nameContent = document.createElement('p')
+        let priceContent = document.createElement('p')
+        let descriptionContent = document.createElement('p')
+
+        nameContent.textContent = data[item]['name'];
+        priceContent.textContent = data[item]['price'];
+        descriptionContent.textContent = data[item]['description'];
+
+        each_product.appendChild(nameContent);
+        each_product.appendChild(priceContent);
+        each_product.appendChild(descriptionContent);
+
+        products.appendChild(each_product);
+    }
+    
+    
+    document.body.appendChild(products);
+}
+
+
+ajax("https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products",
+    function(response){ 
+        render(response);
+    }); // you should get product information in JSON format and render data in the page
---
+++ Week-4/Assugnment-1.py
@@ -0,0 +1,23 @@
+import threading
+from time import sleep
+import time
+
+
+def do_job(number):
+    sleep(3)
+    print(f"Job {number} finished")
+
+
+# rewrite everything inside this main function and keep others untouched
+def main():
+    threads = []
+
+    for i in range(5):
+        t = threading.Thread(target=do_job, args=(i,))
+        threads.append(t)
+
+    for t in threads:
+        t.start()
+
+
+main()
