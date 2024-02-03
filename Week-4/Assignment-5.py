58fabae23b53651c69af9a47dff6dfa5fce638c2
Ray <ray67672@gmail.com>
2024-02-03 23:57:16 +0800

    Update Assignment-5

---
+++ Week-4/Assignment-5.py
@@ -0,0 +1,46 @@
+import time 
+import threading 
+import queue
+
+job_queue = queue.Queue()
+
+for job_index in range(1, 10): # We create 9 jobs with job_index 1 ~ 9
+    job_queue.put(job_index)
+
+class Worker(threading.Thread): 
+    def __init__(self, worker_num):
+        threading.Thread.__init__(self) 
+        self.worker_num = worker_num
+
+    def run(self):
+        # Consumer
+        # Get job from queue
+        # Get job index from queue
+        while True:
+            if job_queue.empty():
+                break
+            else:
+                job_index = job_queue.get()
+                self.do_job(job_index)
+                
+    def do_job(self, index):
+        # Simulate a job (you can image it as doing something which need to take 1 second)
+        print(f"worker {self.worker_num} start job {index}") 
+        time.sleep(1)
+        print(f"worker {self.worker_num} finish job {index}")
+
+workers = []
+worker_count = 3 # We have 3 workers 
+for i in range(worker_count):
+    worker = Worker(i+1)     
+    workers.append(worker)
+
+# Do the job
+# Then put workers on threads
+for worker in workers:
+    worker.start()
+
+for worker in workers: 
+    worker.join()
+
+print("Done.")
